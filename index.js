9712dffb24162cd9c52c0ebb5c75e4a884cde907
NihanShahriarPalock <nihanshahriar000@gmail.com>
2024-05-13 05:40:29 +0600

    JWT Completed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require("jsonwebtoken")
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 9000;
@@ -13,6 +15,24 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+app.use(cookieParser())
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies?.token
+    if (!token) return res.status(401).send({ message: 'Unauthorized access' })
+    if (token) {
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            if (err) {
+                console.log(err)
+                return res.status(401).send({ message: 'Unauthorized access' })
+            }
+            console.log(decoded)
+
+            req.user = decoded
+            next()
+        })
+    }
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lggq9by.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -31,6 +51,29 @@ async function run() {
 
         const addVolunteerCollection = client.db("Volunteer-Management").collection("AddVolunteer")
         const reqVolunteerCollection = client.db("Volunteer-Management").collection("Request Volunteer")
+
+
+
+        app.post("/jwt", async (req, res) => {
+            const user = req.body
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '365d' })
+            res.cookie("token", token, {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict"
+            }).send({ success: true })
+        })
+
+
+        app.get('/logout', (req, res) => {
+           res .clearCookie("token", {
+                httpOnly: true,
+                secure: process.env.NODE_ENV === "production",
+                sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",maxAge:0,
+            }).send({ success: true })
+    
+})
+
 
         app.get('/volunteerNeeds', async (req, res) => {
             const result = await addVolunteerCollection.find().sort({ deadLine: 1 }).toArray();
@@ -57,8 +100,22 @@ async function run() {
             console.log(result);
         })
 
-        app.get('/myPosts/:email', async (req, res) => {
+        app.get('/myPosts/:email', verifyToken, async (req, res) => {
+            const tokenEmail = req.user.email
             const email = req.params.email
+            if (tokenEmail !== email) {
+                return res.status(403).send({ message: 'forbidden access' })
+            }
+            // const token = req?.cookies?.token
+            // if (token) {
+            //     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+            //         if (err) {
+            //            return console.log(err);
+            //         }
+            //         console.log(decoded);
+            //     })
+            // }
+            
             const query = { email: email }
             const result = await addVolunteerCollection.find(query).toArray()
             res.send(result);
@@ -70,6 +127,13 @@ async function run() {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await addVolunteerCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.delete('/reqPost/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await reqVolunteerCollection.deleteOne(query)
             res.send(result)
         })
 
@@ -98,9 +162,9 @@ async function run() {
             console.log(result);
         })
 
-        app.get('/reqFilteredPost/:email', async (req, res) => {
+        app.get('/reqFilteredPost/:postedEmail', async (req, res) => {
             const postedEmail = req.params.postedEmail
-            const query = { email: postedEmail }
+            const query = { postedEmail: postedEmail }
             const result = await reqVolunteerCollection.find(query).toArray()
             res.send(result);
         })
